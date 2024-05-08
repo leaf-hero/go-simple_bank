bdfc0abffd1d85e34ee41625d9ec4cd839f2557d
leaf-hero <helloleaf.vil@gmail.com>
2024-05-08 08:32:27 +0300

    feat(api): create api route for account implement mock test

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"simplebank/api"
+	db "simplebank/db/sqlc"
+	"simplebank/util"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	// Log and check the value and type of config.DBDriver
+	log.Printf("DBDriver value: %v, type: %T\n", config.DBDriver, config.DBDriver)
+
+	// Log and check the value and type of config.DBSource
+	log.Printf("DBSource value: %v, type: %T\n", config.DBSource, config.DBSource)
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
